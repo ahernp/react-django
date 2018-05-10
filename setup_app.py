8124b9e3715764bf7d1bbcbd2948304edda2568f
Paul Ahern <ahernp@ahernp.com>
2018-05-10 20:13:51 +0100

    Got collectstatic working

--- setup_app.py
+++ setup_app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import os
 import subprocess
 
-DB_NAME = os.environ.get('AHERNP_DATABASE_NAME')
-DB_USER = os.environ.get('AHERNP_DATABASE_USER')
-DB_PASSWORD = os.environ.get('AHERNP_DATABASE_PASSWORD')
+with open('.env', 'r') as input_file:
+    lines = input_file.readlines()
+    env_variables = {key: value.strip('"\n') for (key, value) in [line.split('=') for line in lines]}
+
+DB_NAME = env_variables.get('AHERNP_DATABASE_NAME')
+DB_USER = env_variables.get('AHERNP_DATABASE_USER')
+DB_PASSWORD = env_variables.get('AHERNP_DATABASE_PASSWORD')
 
 def run(command):
     try:
@@ -16,7 +19,7 @@ def run(command):
 def setup_postgres():
     sql_execute = 'sudo docker-compose exec db psql -U postgres -c "{command}"'
     commands = [
-        #'CREATE DATABASE {db_name};'.format(db_name=DB_NAME),
+        'CREATE DATABASE {db_name};'.format(db_name=DB_NAME),
         'CREATE USER {db_user} WITH PASSWORD \'{db_password}\';'.format(db_user=DB_USER, db_password=DB_PASSWORD),
         'ALTER ROLE {db_user} SET client_encoding TO \'utf8\';'.format(db_user=DB_USER),
         'ALTER ROLE {db_user} SET default_transaction_isolation TO \'read committed\';'.format(db_user=DB_USER),
@@ -30,7 +33,7 @@ def setup_postgres():
 def setup_webapp():
     execute = 'sudo docker-compose exec webapp python manage.py {command}'
     commands = [
-        'migrate',
+        # 'migrate',
         # 'loaddata project/fixtures/live_snapshot.json',
         # 'poll_feeds --verbose',
         # 'createsuperuser',
